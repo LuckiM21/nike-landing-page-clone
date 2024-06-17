f9b6c7ab3237365a23a961b27d340866765bee08
Luckie Mumba <itxluckym@gmail.com>
2024-06-17 04:50:15 +0200

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nike2.0clone/",
+  base: "/nike-landing-page-clone/",
+  // base: "/nike2.0clone/",
 });
